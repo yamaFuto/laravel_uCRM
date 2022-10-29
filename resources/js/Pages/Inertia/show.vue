c9a99fe6884685202167fd85dc01659f2e292b29
yamaFuto <fuhiroa28155.7575mt@gmail.com>
2022-10-29 18:59:29 +0900

    inertia-vuejs

---
+++ resources/js/Pages/Inertia/show.vue
@@ -0,0 +1,23 @@
+<script setup>
+ import {Inertia} from '@inertiajs/inertia';
+
+defineProps({
+    id : String,
+    blog: Object
+})
+
+const deleteConfirm = (id) => {
+    // console.log(id)
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('ほんとに削除しますか？')
+    })
+}
+</script>
+
+
+<template>
+    {{id}}<br>
+    {{blog.title}}<br>
+    <button @click="deleteConfirm(blog.id)">
+    delete</button>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {Link} from '@inertiajs/inertia-vue3';
+import {ref} from 'vue';
+
+const newTitle = ref('')
+const newContent = ref('')
+</script>
+
+<template>
+    <a href="/">a</a><br>
+    <Link href="/">Link</Link><br>
+    <Link :href="route('inertia.index')">name</Link><br>
+    <Link :href="route('inertia.show', { id: 60 })">route</Link>
+
+
+    <div class="mb-8"></div>
+    <input type="text" name="newTitle" v-model="newTitle"><br>
+    <input type="text" name="newContent" v-model="newContent"><br>
+    <Link as="button" method="post" :href="route('inertia.store')" :data="{
+        title: newTitle,
+        content: newContent
+    }">store</Link>
+</template>
